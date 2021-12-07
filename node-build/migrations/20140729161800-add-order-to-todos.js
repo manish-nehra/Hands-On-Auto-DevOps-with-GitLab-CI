9b4a03b95f54bf897ba32a5b377179178477da00
manish-nehra <61264572+manish-nehra@users.noreply.github.com>
2021-12-07 23:01:08 +0530

    Delete node-build directory

--- node-build/migrations/20140729161800-add-order-to-todos.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(db, callback) {
-  db.addColumn('todos', 'order', { type: 'int' }, callback);
-};
-
-exports.down = function(db, callback) {
-  db.removeColumn('todos', 'order', callback);
-};
